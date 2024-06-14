d86ad9507bf5c884f41b1c8f2663519894e533b2
DedeSitki <sitkicandede@gmail.com>
2024-06-14 10:13:45 +0300

    google maps feature added

--- lib/Screen/BottomNavBarPages/location.dart
+++ lib/Screen/BottomNavBarPages/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -7,12 +8,32 @@ class Location extends StatefulWidget {
   State<Location> createState() => _LocationState();
 }
 
+double _originLatitude = 38.392300;
+double _originLongitude = 27.047840;
+
 class _LocationState extends State<Location> {
+  late GoogleMapController _controller;
+  static final CameraPosition _initalCameraPosition = CameraPosition(
+    target: LatLng(_originLatitude, _originLongitude),
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Location"),
+      body: GoogleMap(
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+        initialCameraPosition: _initalCameraPosition,
+        tiltGesturesEnabled: true,
+        compassEnabled: true,
+        scrollGesturesEnabled: true,
+        zoomGesturesEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          setState(() { // Rebuild the widget with the obtained controller
+            _controller = controller;
+          });
+        },
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,9 @@ import firebase_crashlytics
 import firebase_messaging
 import firebase_remote_config
 import firebase_storage
+import geolocator_apple
 import google_sign_in_ios
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -31,7 +33,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
